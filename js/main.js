151633bdcb17a36c63a1420a0be3fc14b4fc00c7
ignacio alvarado <nash@ignacios-iMac.local>
2023-01-30 22:48:19 -0300

    :art: Home hecho con js

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let artistas = [
+    {imagen: '/src/img/img1.jpg', nombre: 'Productor 1',tipo: 'Productor', ubicacion: 'Viña del mar, Chile', puntuacion: '5/5', descripcionCorta: '', descripcionLarga:''},
+    {imagen: '/src/img/img2.jpg', nombre: 'Productor 2',tipo: 'Productor', ubicacion: 'Santiago, Chile', puntuacion: '4/5', descripcionCorta: '', descripcionLarga:''},
+    {imagen: '/src/img/img3.jpg', nombre: 'Rene Puente',tipo: 'Artista', ubicacion: 'Santiago, Chile', puntuacion: '5/5', descripcionCorta: '', descripcionLarga:''},
+    {imagen: '/src/img/img4.jpg', nombre: 'Artista 2',tipo: 'Artista', ubicacion: 'Buenos aires, Argentina', puntuacion: '5/5', descripcionCorta: '', descripcionLarga:''},
+    {imagen: '/src/img/img5.jpg', nombre: 'Musico 1',tipo: 'Pianista', ubicacion: 'Ottawa, Canada', puntuacion: '5/5', descripcionCorta: '', descripcionLarga:''},
+    {imagen: '/src/img/img6.jpg', nombre: 'Musico 2',tipo: 'Guitarrista', ubicacion: 'Chicago, EEUU', puntuacion: '5/5', descripcionCorta: '', descripcionLarga:''},
+    {imagen: '/src/img/img7.jpg', nombre: 'Musico 3',tipo: 'Tecladista', ubicacion: 'Paris, Francia', puntuacion: '4/5', descripcionCorta: '', descripcionLarga:''}    
+]
+const containerCards = document.querySelector('.container-cards') 
+window.addEventListener('DOMContentLoaded', mostrarArtistas)
+
+function mostrarArtistas(){
+
+    artistas.forEach(artista =>{
+        const card = document.createElement('div')
+        const cardTop = document.createElement('div')
+        const cardBottom = document.createElement('div')
+
+        card.classList.add('card')
+        cardTop.classList.add('card-top')
+        cardBottom.classList.add('card-bottom')
+
+        cardTop.innerHTML = 
+        `
+        <img src="${artista.imagen}">
+        `
+
+        cardBottom.innerHTML = 
+        `
+        <h5>${artista.nombre}</h5>
+        <p>${artista.tipo}</p>
+        <p>${artista.ubicacion}</p>
+        <p>${artista.puntuacion}</p>
+        `
+        card.appendChild(cardTop)
+        card.appendChild(cardBottom)
+
+        containerCards.appendChild(card)
+
+    })
+}
